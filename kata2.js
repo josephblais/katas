b8f0727cc74fa909f9c1051c659b4aad9f475786
Joseph Blais <josephpierreblais@gmail.com>
2020-12-30 17:12:28 +0000

    conditional sums

---
+++ kata2.js
@@ -0,0 +1,18 @@
+  const conditionalSum = function(values, condition) {
+    let conditionalValuesSum = 0;
+    for (let value of values) {
+      if (condition === "even" && value % 2 === 0) {
+        conditionalValuesSum += value;
+      }
+      else if (condition === "odd" && value % 2 !== 0) {
+        conditionalValuesSum += value;
+      }
+    }
+    return conditionalValuesSum;
+  };
+
+
+console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
+console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
+console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
+console.log(conditionalSum([], "odd"));
